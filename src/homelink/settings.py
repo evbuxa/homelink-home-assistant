8ab6ca226f550995c55b7013a8254ce2eed99292
Nicholas Aelick <niaexa@syntronic.com>
2025-01-15 21:23:50 +0000

    Merged PR 53108: Update to new state url
    Use the new state url for requests
    
    Related work items: #75634

--- src/homelink/settings.py
+++ src/homelink/settings.py
@@ -1,16 +1,17 @@
 from decouple import config
 
 # TODO: Defaults should be production
-HOST_URL = config("HOMELINK_HOST_URL", default="https://homelinkcloud.com")
+HOST_URL = config("HOMELINK_HOST_URL", default="homelinkcloud.com")
 DISCOVER_URL = config(
     "HOMELINK_DISCOVER_URL",
-    default=f"{HOST_URL}/services/v2/home-assistant/fulfillment",
+    default=f"https://{HOST_URL}/services/v2/home-assistant/fulfillment",
 )
 ENABLE_URL = config(
-    "HOMELINK_ENABLE_URL", default=f"{HOST_URL}/services/v2/home-assistant/fulfillment"
+    "HOMELINK_ENABLE_URL",
+    default=f"https://{HOST_URL}/services/v2/home-assistant/fulfillment",
 )
 STATE_URL = config(
-    "HOMELINK_STATE_URL", default=f"{HOST_URL}/services/v2/home-assistant/state"
+    "HOMELINK_STATE_URL", default="https://state.{HOST_URL}/services/v2/home-assistant"
 )
 
 COGNITO_POOL_ID = config("COGNITO_POOL_ID", default="us-east-2_sBYr2OD1J")
